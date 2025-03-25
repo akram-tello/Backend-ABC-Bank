2eb37a5f4511d200aef159fb36aeeb2803f9ff64
Akram Tello <akramttello@gmail.com>
2025-03-25 11:18:45 +0800

    config url for production

--- src/index.js
+++ src/index.js
@@ -18,7 +18,12 @@ const userController = new UserController();
 const transactionController = new TransactionController();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://fe-abc.onrender.com', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Routes
